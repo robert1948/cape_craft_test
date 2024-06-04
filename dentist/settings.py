2c8fd5f32bd99efcff9011e9650a5c140f9cc816
Robert Kleyn <zeonita@gmail.com>
2024-06-05 01:38:37 +0200

    Mail commit message

--- dentist/settings.py
+++ dentist/settings.py
@@ -126,7 +126,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'johne4196@gmail.com'
 EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 # EMAIL_USE_SSL = False 
 
 
