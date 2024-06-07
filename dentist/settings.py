d6404f5b5ed5820035aa216f278898ca22bb3111
Robert Kleyn <zeonita@gmail.com>
2024-06-08 01:47:35 +0200

    Update ALLOWED_HOSTS in settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '=c_^67@&4il)k1r!#7uwygqk@xbx14f2sz*z5=-mc@05nm6bwa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.cape-control.com', 'cape-control.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','www.cape-control.com', 'cape-control.com']
 
 # Application definition
 
